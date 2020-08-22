5b62f83d1851941fb7d43f3a61e056b765bc5582
TjandraD <tjandradarmo@protonmail.com>
2020-08-22 20:07:01 +0700

    Remove unused widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,39 +4,6 @@ import 'components/countdown_timer.dart';
 
 void main() => runApp(HomePage());
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: Colors.grey.shade300,
-//         body: Column(
-//           children: [
-//             SafeArea(
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.end,
-//                 children: [
-//                   Container(
-//                     margin: EdgeInsets.only(top: 16, right: 16),
-//                     width: 50,
-//                     height: 50,
-//                     child: SwitchWidget(),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             Expanded(
-//               child: Container(
-//                 child: CountdownTimer(),
-//               ), // the countdown goes here
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();

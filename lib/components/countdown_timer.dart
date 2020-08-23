97faa155268347036229cc4b14f71816c9252660
Erick Christian <erickchristian48@gmail.com>
2020-08-23 10:41:19 +0700

    Changed to a stateless widget

--- lib/components/countdown_timer.dart
+++ lib/components/countdown_timer.dart
@@ -2,27 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:slide_countdown_clock/slide_countdown_clock.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
-class CountdownTimer extends StatefulWidget {
-  @override
-  _CountdownTimerState createState() => _CountdownTimerState();
-}
+class CountdownTimer extends StatelessWidget {
+  final Duration duration;
+  final Function setTimerState;
 
-class _CountdownTimerState extends State<CountdownTimer> {
+  CountdownTimer(this.duration, this.setTimerState);
+
   @override
   Widget build(BuildContext context) {
     return SlideCountdownClock(
-      duration: Duration(minutes: 30),
+      duration: this.duration,
       textStyle: TextStyle(
         fontSize: 40,
       ),
       separator: ":",
       onDone: () {
         Alert(
-                context: context,
-                title: 'Indonesia Maju Hackfest',
-                desc: 'It\'s 30 minutes already! \nLet\'s wash your hands',
-                image: Image.asset('assets/hand-wash.png'))
-            .show();
+            context: context,
+            title: 'Indonesia Maju Hackfest',
+            desc: 'It\'s 30 minutes already! \nLet\'s wash your hands',
+            image: Image.asset('assets/hand-wash.png'),
+            closeFunction: () {
+              this.setTimerState(false);
+              Future.delayed(
+                  Duration(milliseconds: 500), () => this.setTimerState(true));
+            }).show();
       },
     );
   }
